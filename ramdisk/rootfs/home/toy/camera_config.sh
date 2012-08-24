702562773eb2916684f26d2c82ebadccb09f9337
薛继龙 <53612244@qq.com>
2012-08-24 14:29:28 +0800

    [pcare] 修改camera配置

--- ramdisk/rootfs/home/toy/camera_config.sh
+++ ramdisk/rootfs/home/toy/camera_config.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
-/home/toy/camera_config Brightness 165
-/home/toy/camera_config Contrast 80
-/home/toy/camera_config Saturation 150
-/home/toy/camera_config Hue 0
-/home/toy/camera_config Gamma 65
-/home/toy/camera_config PLF 1
-/home/toy/camera_config WBT 5094
-/home/toy/camera_config Sharpness 9
-/home/toy/camera_config "Backlight Compensation" 4
+/home/toy/camera_config Brightness 140
+/home/toy/camera_config Contrast 50
+#/home/toy/camera_config Saturation 150
+#/home/toy/camera_config Hue 0
+#/home/toy/camera_config Gamma 65
+#/home/toy/camera_config PLF 1
+#/home/toy/camera_config WBT 5094
+#/home/toy/camera_config Sharpness 9
+#/home/toy/camera_config "Backlight Compensation" 4
