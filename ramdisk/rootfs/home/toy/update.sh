ab54f739fbe037d95a5681b0d871c82972771607
薛继龙 <xuejilong@foxmail.com>
2012-09-05 10:46:02 +0800

    [pcare] wifi更新脚本添加条件判断，降低kernel升级失败的概率[D

--- ramdisk/rootfs/home/toy/update.sh
+++ ramdisk/rootfs/home/toy/update.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
-tftpd -c /tmp
-mtd_debug erase /dev/mtd1 0 0x800000
-nandwrite -p /dev/mtd1 /tmp/uImage
-reboot
+tftpd -c /tmp  2> /log.txt
+log=`busybox du -k /log.txt|busybox awk '{print $1}'`
+uimg=`busybox du -m /tmp/uImage|busybox awk '{print $1}'`
+if [ $log -gt 1 ]; then
+	rm -rf log.txt
+elif [ $uimg -gt 5 ]; then
+	rm -rf log.txt                       
+	mtd_debug erase /dev/mtd1 0 0x800000 
+	nandwrite -p /dev/mtd1 /tmp/uImage 
+	reboot 
+fi
