0b71434f3176333c710194af05975315f0a969e1
薛继龙 <xuejilong@foxmail.com>
2012-10-22 14:40:44 +0800

    [kernel & p-care] 打开所有的串口打印信息；更新版本号至0.2.1.3

--- pcare/src/network/network.c
+++ pcare/src/network/network.c
@@ -203,7 +203,7 @@ char str_ctl[]	 = "MO_O";
 char str_data[]  = "MO_V";
 char str_camID[] = "yuanxiang7501";
 char str_SSID[16];
-char str_camVS[] = {0, 2, 1, 2};
+char str_camVS[] = {0, 2, 1, 3};
 char str_tmp[14];
 
 /* ---------------------------------------------------------- */
