8da1e2403ed2aea56eef56ec21b4acdfa813cf52
薛继龙 <xuejilong@foxmail.com>
2012-10-22 14:59:07 +0800

    [p-care] 给led_control部分加上锁，除去led操作中的隐患

--- pcare/src/control/led_control.c
+++ pcare/src/control/led_control.c
@@ -4,24 +4,29 @@
 #include <string.h>
 #include <sys/time.h>
 #include <fcntl.h>
+#include <pthread.h>
 //#include "syswait.h"
 
 char *name = "/sys/devices/platform/sep0611_led.1/leds/LED_Statue/brightness";
 //struct itimerval tick;
 int led_flag =1;
+pthread_mutex_t led_mutex_lock = PTHREAD_MUTEX_INITIALIZER;
 
 void led_on()
 {
 	char buf[2] = "1";
     int fd,size;
+    pthread_mutex_lock(&led_mutex_lock);
     fd = open(name, O_RDWR);
     if(fd == 0)
     {
         printf("led open error\n");
+        pthread_mutex_unlock(&led_mutex_lock);
         return;
     }
     write(fd, buf, strlen(buf));
     close(fd);
+    pthread_mutex_unlock(&led_mutex_lock);
     return;
 }
 
@@ -29,14 +34,17 @@ void led_off()
 {
 	char buf[2]="0";
 	int fd,size;
+    pthread_mutex_lock(&led_mutex_lock);
 	fd = open(name, O_WRONLY);
     if(fd == 0)
     {
         printf("led open error\n");
+        pthread_mutex_unlock(&led_mutex_lock);
         return;
     }
     write(fd, buf, strlen(buf));
     close(fd);
+    pthread_mutex_unlock(&led_mutex_lock);
 	return;
 }
 
@@ -44,14 +52,20 @@ void led_flash()
 {
     pthread_detach(pthread_self());
 	while(1){
-		led_on();
-		sleep(2);
-		if(!led_flag)
-			break;
-		led_off();
-		sleep(2);
-		if(!led_flag)
-			break;
+        if(!led_flag){
+            break;
+        }
+        else{
+            led_on();
+            sleep(2);
+        }
+        if(!led_flag){
+            break;
+        }
+        else{
+            led_off();
+            sleep(2);
+        }
 	}
 	return;
 }
