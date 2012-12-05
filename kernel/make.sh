7c2c5a1e130a5316703f99ddcb15ead2c4092df3
薛继龙 <xuejilong@foxmail.com>
2012-12-05 11:22:51 +0800

    更新kernel编译脚本

--- kernel/make.sh
+++ kernel/make.sh
@@ -3,5 +3,10 @@ rm arch/unicore/boot/uImage
 cd usr
 rm built-in.o .built-in.o.cmd .gen_init_cpio.cmd initramfs_data.cpio .initramfs_data.cpio.cmd .initramfs_data.cpio.d initramfs_data.o .initramfs_data.o.cmd
 cd ..
+make pcare_defconfig
+make uImage -j4
+cd drivers/wifi-ap/
+./make.sh
+cd ../../
 make uImage -j4
 #cp arch/unicore/boot/uImage ../pcare/
